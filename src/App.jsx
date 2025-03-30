b0bb31e22c574ac5f5c6d18f4fa901087a522146
alison.lemes.p9 <alison.lemes.p9@gmail.com>
2025-03-30 23:04:29 +0000

    finalização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -52,11 +52,17 @@ function handleChangeRateNote(value){
       </div>
      ) : (
 
-      <div className="bg-radial-gradient max-w-103  px-6 pt-6 pb-8 rounded-2xl font-overpass ">
+      <div className="bg-radial-gradient max-w-103  px-6 pt-6 pb-8 rounded-2xl font-overpass text-center ">
     
-      <img src={illustration-thank-you.svg} alt="illustration thank you" />
+      <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
 
-      <p></p>
+      <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl"  >You selected {rateNote} out of 5!</p>
+
+      
+      <h1 className="font-bold text-2xl mb-2.5   " >Thank You!</h1>
+       <p className="text-light-grey  text-sn  leading-1  ">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+ 
+
 
       </div>
 
